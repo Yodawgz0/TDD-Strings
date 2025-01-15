3b3c4f2619b8ef32eac54ff28fc0a9f0d13e7ff9
Ashley <49788162+Yodawgz0@users.noreply.github.com>
2025-01-15 18:06:25 +0000

    my-thougthts

--- src/StringCalc.ts
+++ src/StringCalc.ts
@@ -1,5 +1,14 @@
-// Creating the class StringCalc to process the information and setting the input as the string
 
+// My thought after this is we can definitely use regex heavily for the process but i feel
+// sometimes it is good to have it but they are hard to read and understand as there are no descriptive jargon attached to item
+// more or less i have also observed that the regex is slow on big data sets and that is why i have tried working with data manupilation 
+// but again there is a regex which will solve it but who actually knows regex is another question
+
+// It was interesitng to solve this one as honestly being a tester for my early career, i can think of many more test cases i can work with 
+// but since the question had guidelines so i did no go above and beyond as there is an equilibrium to be maintained.
+
+
+// Creating the class StringCalc to process the information and setting the input as the string
 export class StringCalc {
   add = (numberStr: string) => {
     // This will take care of the empty string
