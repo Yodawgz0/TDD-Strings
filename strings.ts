f6e7577551f5902bce8ddf84f71ff0c24c038a39
Ashley <49788162+Yodawgz0@users.noreply.github.com>
2025-01-15 20:14:58 +0530

    handling of the single number created

--- strings.ts
+++ strings.ts
@@ -1,4 +1,20 @@
 // Creating the function add to process the information and setting the input as the string
-const add = (number: string) => {
-    
+const add = (numberStr: string) => {
+    // using try catch to parse the number from the string , 
+    // if it fails then we can return an error
+    try {
+        const numberParsed = parseInt(numberStr)
+        return numberParsed
+    } catch (err) {
+        console.log(err)
+        return "Unable to extract the number out of the string"
+    }
 }
+
+// input data defined
+const inputString = "1"
+
+// Calling the function
+const processedData = add(inputString)
+
+console.log(processedData)
