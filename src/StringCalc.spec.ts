bb3e776965961c1219944f7b2dd08120f6b5d980
Ashley <49788162+Yodawgz0@users.noreply.github.com>
2025-01-15 15:47:44 +0000

    test2-return-as-it-is

--- src/StringCalc.spec.ts
+++ src/StringCalc.spec.ts
@@ -5,8 +5,13 @@ describe("StringCalc", () => {
   beforeEach(() => {
     strcalc = new StringCalc();
   });
-
+  // Test 0
   it("should return 0 for an empty string", () => {
     expect(strcalc.add("")).toBe(0);
+  });
+  // Test 1
+  it("should return the number itself if the string contains one number", () => {
+    expect(strcalc.add("1")).toBe(1);
+    expect(strcalc.add("5")).toBe(5);
   });
 });
